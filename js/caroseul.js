890428e1e46bda7ce091edf844e33310f06ac267
Zoraxis <b.v.o0077@gmail.com>
2024-02-20 21:32:18 +0100

    ultra animation

--- js/caroseul.js
+++ js/caroseul.js
@@ -85,13 +85,36 @@ const initCaroseul = () => {
     });
   });
 
-  if (window.screen.width > mobileMax) {
-    // caroseul.children[1].children[0].style.height = "67vh";
-    // caroseul.children[1].children[0].style.transform = "translateY(-4vh)";
-  }
+  CaruselResizeHandle();
 
   setCareoseulLink();
 };
+
+const CaruselResizeHandle = () => {
+  if (window.screen.width > mobileMax) {
+    var originalRatios = {
+      width: (containerX * 1) / 2880,
+      height: (containerY * 1.1) / 1700,
+    };
+
+    var coverRatio = Math.max(originalRatios.width, originalRatios.height);
+
+    var newImageWidth = 2880 * coverRatio;
+    var newImageHeight = 1700 * coverRatio;
+
+    caroseul.children[1].children[0].style.transformOrigin = "left top";
+    caroseul.children[1].children[0].style.left = "50%";
+    caroseul.children[1].children[0].style.top = `-${0}vh`;
+    caroseul.children[1].children[0].style.height =`${newImageHeight}px`;
+    caroseul.children[1].children[0].style.width = `${newImageWidth}px`;
+    caroseul.children[1].children[0].style.maxWidth = `${newImageWidth}px`
+
+    const carouselCenterYOffset = newImageHeight * 0.6 - containerY * 0.62;
+    caroseul.children[1].children[0].style.transform = `scale(0.6) translateX(-49.5%) translateY(-${carouselCenterYOffset / 6 * 10}px)`;
+  }
+}
+
+window.addEventListener("resize", CaruselResizeHandle);
 
 const callCarouselAction = (target) => {
   if (getQueued("caroseul")) return;
